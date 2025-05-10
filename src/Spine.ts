ea781503d463b193a12a1024b2d3bee8329f9ad1
yjgaia <yjdev88@gmail.com>
2025-05-10 13:46:27 +0900

    Bump version to 0.3.6 and update dependencies; refactor import order and improve condition check in Spine class

--- src/Spine.ts
+++ src/Spine.ts
@@ -1,10 +1,10 @@
 import {
   AtlasAttachmentLoader,
-  Spine as PixiSpine,
   SkeletonBinary,
   SkeletonData,
   SkeletonJson,
   Skin as SpineSkin,
+  Spine as PixiSpine,
   SpineTexture,
   TextureAtlas,
 } from "@esotericsoftware/spine-pixi-v8";
@@ -87,7 +87,7 @@ export default class Spine extends GameObject {
 
     await Promise.all(promises);
 
-    if ((!texture && !textures) || this.removed) return;
+    if ((!texture && !textures) || this.isRemoved()) return;
 
     const atlas = new TextureAtlas(textAtlasData!);
     atlas.pages.forEach((page) => {
